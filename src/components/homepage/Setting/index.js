c78230cef40a06ceb38406934c398c8533725365
hieptvh18 <hieptvh18@gmail.com>
2023-02-25 00:31:23 +0700

    update toggle mode theme

--- src/components/homepage/Setting/index.js
+++ src/components/homepage/Setting/index.js
@@ -1,28 +1,41 @@
 import "./style.scss";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp, faGears, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp, faGears, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Settings = (props) => {
   // handle click show content setting
   const [isContentSettingActive, setContentSettingActive] = useState(false);
-  useEffect(() => {
-    const settingBtn = document.querySelector(".settings-icon");
-    const settingContent = document.querySelector(".setting-content");
-  },[]);
-
+  const [isDarkMode, setDarkMode] = useState(true);
+  
   const handleShowSetting = (e) => {
     setContentSettingActive(!isContentSettingActive);
   };
 
+  const handleMode = (e)=>{
+    setDarkMode(!isDarkMode);
+  }
+
   return (
     <div className={isContentSettingActive ? "settings active" : "settings"}>
-      <FontAwesomeIcon
-        onClick={handleShowSetting}
-        className="settings-icon"
-        icon={faGears}
-        color="#fff"
-      />
+      <div className="settings-left">
+        <div className="settings-left__icon-settings">
+          <FontAwesomeIcon
+            onClick={handleShowSetting}
+            className="settings-icon"
+            icon={faGears}
+            color="#fff"
+          />
+        </div>
+        <div className={isDarkMode ? 'setting-left__mode dark' : 'setting-left__mode light'}>
+          <FontAwesomeIcon
+            onClick={handleMode}
+            className="mode-icon"
+            icon={isDarkMode ? faMoon : faSun}
+            color="#fff"
+          />
+        </div>
+      </div>
       <div className="setting-content">
         <div className="theme-mode">
           <div className="theme-mode__title">Change Mode</div>
