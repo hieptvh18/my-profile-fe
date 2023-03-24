47bc7fe3a02d65937a8f6612188a014542144193
hieptvh18 <hieptvh18@gmail.com>
2023-03-24 23:22:56 +0700

    update profile page

--- src/api/User.js
+++ src/api/User.js
@@ -5,8 +5,14 @@ export const getUser = ()=>{
     return Instance.get(url);
 }
 
-
+// auth login admin page
 export const authLogin = (formData)=>{
     const url = '/login-post';
+    return Instance.post(url,formData);
+}
+
+// update user data
+export const updateUser = (formData)=>{
+    const url = '/update-user';
     return Instance.post(url,formData);
 }
--- src/components/admin/Profile/index.js
+++ src/components/admin/Profile/index.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { getUser } from "../../../api/User";
 import { image } from "@cloudinary/url-gen/qualifiers/source";
+import { updateUser } from "../../../api/User";
 
 const Profile = () => {
   const [user, setUser] = useState({});
   const [avatar, setAvatar] = useState("");
-  const [avatarInpt, setAvatarInpt] = useState("");
   const [errors, setErrors] = useState({name:'',email:''});
+  const [message,setMessage] = useState({});
 
   useEffect(() => {
     const getUserData = async () => {
@@ -36,13 +37,30 @@ const Profile = () => {
 
     if(validateForm(formProps)){
       // handle call api update info
-      console.log('update infomation')
+      console.log('update infomation');
+      
+      if(formProps.avatar.size === 0){
+        delete formProps.avatar;
+      }
+      if(formProps.cv.size === 0){
+        delete formProps.cv;
+      }
+
+      console.log(formProps);
+
+      updateUser(formProps)
+      .then(res=>{
+        console.log(res)
+        setMessage({message:res.data.message,status:res.data.status})
+      })
+      .catch(err=>{
+        console.log(err)
+      })
     }
   };
 
   // validate form input
   const validateForm = (formData) => {
-    console.log(formData);
     let flag = true;
     let errMsg = {};
     const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -75,25 +93,36 @@ const Profile = () => {
     }
 
     // phone
+    let phonePattern = /\d{10,15}$/
+    if (!formData.phone) {
+      errMsg.phone = 'Field phone is requried!';
+    }
+    else if(!phonePattern.test(formData.phone)){
+      errMsg.phone = 'Phone is invalid!';
+    }
+    else {
+      errMsg.phone = '';
+    }
 
     // password
-    if (!formData.password) {
-      errMsg.password = 'Field password is requried!';
-    }
-    else if(!passwordPattern.test(formData.password)){
-      errMsg.password = 'Password is invalid(6-30 character)!';
-    } else {
-      errMsg.password = '';
-    }
+    // if (!formData.password) {
+    //   errMsg.password = 'Field password is requried!';
+    // }
+    // else if(!passwordPattern.test(formData.password)){
+    //   errMsg.password = 'Password is invalid(6-30 character)!';
+    // } else {
+    //   errMsg.password = '';
+    // }
 
     // avatar
     let exImg = ['image/jpg','image/png','image/jpeg'];
+    let maxFileSize = 1024 * 1024 * 3; // 3mb-> bytes
     if(formData.avatar.size > 0 && !exImg.includes(formData.avatar.type)){
       errMsg.avatar = 'avatar is should be png,jpg,jpeg file!';
     }
-    // else if(formData.avatar.size > 2024){
-    //   errMsg.avatar = 'Avatar size should be < 2mb';
-    // }
+    else if(formData.avatar.size > 0 && formData.avatar.size >= maxFileSize){
+      errMsg.avatar = 'Avatar size should be < 2mb';
+    }
     else{
       errMsg.avatar = '';
     }
@@ -101,7 +130,11 @@ const Profile = () => {
     // cv
     if(formData.cv.size > 0 && formData.cv.type !== 'application/pdf'){
       errMsg.cv = 'CV is should be PDF file!';
-    }else{
+    }
+    else if(formData.cv.size > 0 && formData.cv.size >= maxFileSize){
+      errMsg.cv = 'CV size should be < 2mb';
+    }
+    else{
       errMsg.cv = '';
     }
 
@@ -120,9 +153,32 @@ const Profile = () => {
     return arrMsgFilter.length;
   }
 
+  // upload file to cloundainary->get link cdn
+  const uploadFileCnd = (file)=>{
+
+  }
+
+  const MessageElemnts = ()=>{
+
+    let el = '';
+
+    if(message.status && message.status === 'success'){
+      el = <div className="alert alert-success">{message.message}</div>
+    }else if(message.status && message.status === 'error'){
+      el = <div className="alert alert-danger">{message.message}</div>
+    }
+    
+    return (
+      <div>{el}</div>
+    )
+  };
+
   return (
     <section style={{ backgroundColor: "#fff", padding: "10px" }}>
       <h2>Profile manage</h2>
+
+      {message.status && <MessageElemnts/>}
+    
       <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
         <div className="form-group row">
           <div className="mb-3 col-6">
@@ -164,10 +220,11 @@ const Profile = () => {
             <input
               defaultValue={user.phone ?? user.phone}
               type="text"
-              name="avatar"
+              name="phone"
               className="form-control"
               id="phone"
             />
+              {errors.phone && <small className="text-danger">{errors.phone}</small>}
           </div>
           <div className="mb-3 col-6">
             <label htmlFor="age" className="form-label">
@@ -219,7 +276,7 @@ const Profile = () => {
           {errors.cv && <small className="text-danger">{errors.cv}</small>}
           <div className="display-cv"></div>
         </div>
-        <div className="mb-3">
+        {/* <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
@@ -234,7 +291,7 @@ const Profile = () => {
           <div id="emailHelp" className="form-text">
             You shouldn't share your password with anyone else.
           </div>
-        </div>
+        </div> */}
 
         <button type="submit" className="btn btn-primary">
           Update
