a6a81ccd0638582ae16d08ac59be6fde1dea5eb6
hieptvh <hiep.tranhoang@pt-corp.vn>
2023-11-08 21:31:16 +0700

    uninstall cloudainary

--- src/pages/client/Error/PageNotFound/index.js
+++ src/pages/client/Error/PageNotFound/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import '../PageNotFound/style.scss';
 
 const PageNotFound = ()=>{
-    return (<div>404</div>)
+    return (
+        <div className="page-404">
+            <a href="/">Go to homepage</a>
+        </div>
+    )
 }
 
 export default PageNotFound;
