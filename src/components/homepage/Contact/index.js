1a0a87857ffb7304ec172ee87fd44433d3e416ce
hieptvh <hiep.tranhoang@pt-corp.vn>
2023-11-09 16:15:17 +0700

    install react-reval animation

--- src/components/homepage/Contact/index.js
+++ src/components/homepage/Contact/index.js
@@ -3,6 +3,7 @@ import './style.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot,faEnvelope,faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faMailchimp, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+import { Fade } from "react-reveal";
 
 const Contact = (props)=>{
     return (<section id="contact">
@@ -15,77 +16,81 @@ const Contact = (props)=>{
       </div>
     </div>
     <div className="row contact-content">
-      <div className="form-contact col-12 col-xs-12 col-sm-12 col-md-7 col-lg-7">
-        <h4 className="font-weight-bold text-center mb-5">Get In Touch</h4>
-        <form action="" method="POST">
-          <div className="row">
-            <div className="form-group col-12 col-sm-12 col-md-6 col-lg-6">
-              <input
-                type="text"
-                className=""
-                name="name"
-                placeholder="Your Name"
-                required
-              />
+      <Fade left>
+        <div className="form-contact col-12 col-xs-12 col-sm-12 col-md-7 col-lg-7">
+          <h4 className="font-weight-bold text-center mb-5">Get In Touch</h4>
+          <form action="" method="POST">
+            <div className="row">
+              <div className="form-group col-12 col-sm-12 col-md-6 col-lg-6">
+                <input
+                  type="text"
+                  className=""
+                  name="name"
+                  placeholder="Your Name"
+                  required
+                />
+              </div>
+              <div className="form-group col-12 col-sm-12 col-md-6 col-lg-6">
+                <input
+                  type="text"
+                  className=""
+                  name="email"
+                  placeholder="Your Email"
+                  required
+                />
+              </div>
             </div>
-            <div className="form-group col-12 col-sm-12 col-md-6 col-lg-6">
-              <input
-                type="text"
-                className=""
-                name="email"
-                placeholder="Your Email"
+            <div className="form-group">
+              <input type="tel" name="phone" placeholder="Your Phone" />
+            </div>
+            <div className="form-group">
+              <select name="service" id="service-input">
+                <option value="">Choose a Service</option>
+                <option value="Web design">Web Design</option>
+                <option value="PHP Developer">PHP Developer</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <textarea
+                name="message"
+                id="message-contact"
+                cols="30"
+                rows="8"
+                placeholder="Message"
                 required
-              />
+              ></textarea>
+            </div>
+            <button className="btn-submit-contact mt-4" type="submit">
+              Submit Now
+            </button>
+          </form>
+        </div>
+      </Fade>
+      <Fade right>
+        <div className="contact__right col-12 col-md-5 col-lg-5">
+          <div className="contact-right__item">
+            <h4 className="contact-item__title">155 Cau Giay, Ha Noi</h4>
+            <div className="d-flex align-items-center">
+              <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faLocationDot}/></div>
+              <span className="sub-title_contact-title">Address</span>
             </div>
           </div>
-          <div className="form-group">
-            <input type="tel" name="phone" placeholder="Your Phone" />
+          <div className="contact-right__item">
+            <h4 className="contact-item__title">hieptvh18@gmail.com</h4>
+            <div className="d-flex align-items-center">
+              <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faEnvelope} /></div>
+              <span className="sub-title_contact-title">Email</span>
+            </div>
           </div>
-          <div className="form-group">
-            <select name="service" id="service-input">
-              <option value="">Choose a Service</option>
-              <option value="Web design">Web Design</option>
-              <option value="PHP Developer">PHP Developer</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <textarea
-              name="message"
-              id="message-contact"
-              cols="30"
-              rows="8"
-              placeholder="Message"
-              required
-            ></textarea>
-          </div>
-          <button className="btn-submit-contact mt-4" type="submit">
-            Submit Now
-          </button>
-        </form>
-      </div>
-      <div className="contact__right col-12 col-md-5 col-lg-5">
-        <div className="contact-right__item">
-          <h4 className="contact-item__title">155 Cau Giay, Ha Noi</h4>
-          <div className="d-flex align-items-center">
-            <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faLocationDot}/></div>
-            <span className="sub-title_contact-title">Address</span>
+          <div className="contact-right__item">
+            <h4 className="contact-item__title">+84 89581167</h4>
+            <div className="d-flex align-items-center">
+              <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faPhone}/></div>
+              <span className="sub-title_contact-title">Phone Number</span>
+            </div>
           </div>
         </div>
-        <div className="contact-right__item">
-          <h4 className="contact-item__title">hieptvh18@gmail.com</h4>
-          <div className="d-flex align-items-center">
-            <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faEnvelope} /></div>
-            <span className="sub-title_contact-title">Email</span>
-          </div>
-        </div>
-        <div className="contact-right__item">
-          <h4 className="contact-item__title">+84 89581167</h4>
-          <div className="d-flex align-items-center">
-            <div className="icon icon-address"><FontAwesomeIcon color="#fff" fontSize={"40px"} icon={faPhone}/></div>
-            <span className="sub-title_contact-title">Phone Number</span>
-          </div>
-        </div>
-      </div>
+      </Fade>
     </div>
   </section>)
 }
--- src/components/homepage/NewsWidget/index.js
+++ src/components/homepage/NewsWidget/index.js
@@ -9,6 +9,7 @@ import "swiper/css";
 // import required modules
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
+import { Fade } from "react-reveal";
 
 const NewsWidget = (props) => {
   return (
@@ -51,154 +52,157 @@ const NewsWidget = (props) => {
             </ul>
           </div>
         </div>
-        <div className="blog-list">
-          <Swiper
-          modules={[Pagination]}
-          pagination={{
-            clickable: true,
-          }}
-            spaceBetween={50}
-            slidesPerView={4}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-            // responsive
-            breakpoints={{
-              390:{
-                slidesPerView:1,
-              },
-              576: {
-                // width: 576,
-                slidesPerView: 2,
-              },
-              768: {
-                // width: 768,
-                slidesPerView: 3,
-              },
-              1200: {
-                // width: 768,
-                slidesPerView: 4,
-              },
+
+        <Fade left>
+          <div className="blog-list">
+            <Swiper
+            modules={[Pagination]}
+            pagination={{
+              clickable: true,
             }}
-          >
-            <SwiperSlide>
-              <div className="blog-item">
-                <div className="img-blog">
-                  <img
-                    src="https://assets.materialup.com/uploads/6ab5f964-2a06-4913-9826-0c928a9dd091/preview.png"
-                    alt="blog"
-                  />
-                </div>
-                <div className="blog-item__content">
-                  <div className="blog-item__tag">
-                    <span className="tag__category">Design</span>
-                    <span className="tag__created-at">
-                      <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
-                    </span>
+              spaceBetween={50}
+              slidesPerView={4}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+              // responsive
+              breakpoints={{
+                390:{
+                  slidesPerView:1,
+                },
+                576: {
+                  // width: 576,
+                  slidesPerView: 2,
+                },
+                768: {
+                  // width: 768,
+                  slidesPerView: 3,
+                },
+                1200: {
+                  // width: 768,
+                  slidesPerView: 4,
+                },
+              }}
+            >
+              <SwiperSlide>
+                <div className="blog-item">
+                  <div className="img-blog">
+                    <img
+                      src="https://assets.materialup.com/uploads/6ab5f964-2a06-4913-9826-0c928a9dd091/preview.png"
+                      alt="blog"
+                    />
                   </div>
+                  <div className="blog-item__content">
+                    <div className="blog-item__tag">
+                      <span className="tag__category">Design</span>
+                      <span className="tag__created-at">
+                        <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
+                      </span>
+                    </div>
 
-                  <h3 className="blog-title">Become fullstack developer</h3>
-                  <a href="" className="blog-detail-btn">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="blog-item">
-                <div className="img-blog">
-                  <img
-                    src="https://assets.materialup.com/uploads/013c048d-b659-4bb9-9ec2-bd2e9355ef63/preview.png"
-                    alt="blog"
-                  />
-                </div>
-                <div className="blog-item__content">
-                  <div className="blog-item__tag">
-                    <span className="tag__category">Design</span>
-                    <span className="tag__created-at">
-                      <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
-                    </span>
+                    <h3 className="blog-title">Become fullstack developer</h3>
+                    <a href="" className="blog-detail-btn">
+                      Read More
+                    </a>
                   </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="blog-item">
+                  <div className="img-blog">
+                    <img
+                      src="https://assets.materialup.com/uploads/013c048d-b659-4bb9-9ec2-bd2e9355ef63/preview.png"
+                      alt="blog"
+                    />
+                  </div>
+                  <div className="blog-item__content">
+                    <div className="blog-item__tag">
+                      <span className="tag__category">Design</span>
+                      <span className="tag__created-at">
+                        <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
+                      </span>
+                    </div>
 
-                  <h3 className="blog-title">Become fullstack developer</h3>
-                  <a href="" className="blog-detail-btn">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              {" "}
-              <div className="blog-item">
-                <div className="img-blog">
-                  <img
-                    src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
-                    alt="blog"
-                  />
-                </div>
-                <div className="blog-item__content">
-                  <div className="blog-item__tag">
-                    <span className="tag__category">Design</span>
-                    <span className="tag__created-at">
-                      <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
-                    </span>
+                    <h3 className="blog-title">Become fullstack developer</h3>
+                    <a href="" className="blog-detail-btn">
+                      Read More
+                    </a>
                   </div>
-                  <h3 className="blog-title">Become fullstack developer</h3>
-                  <a href="" className="blog-detail-btn">
-                    Read More
-                  </a>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              {" "}
-              <div className="blog-item">
-                <div className="img-blog">
-                  <img
-                    src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
-                    alt="blog"
-                  />
-                </div>
-                <div className="blog-item__content">
-                  <div className="blog-item__tag">
-                    <span className="tag__category">Design</span>
-                    <span className="tag__created-at">
-                      <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
-                    </span>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <div className="blog-item">
+                  <div className="img-blog">
+                    <img
+                      src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
+                      alt="blog"
+                    />
                   </div>
+                  <div className="blog-item__content">
+                    <div className="blog-item__tag">
+                      <span className="tag__category">Design</span>
+                      <span className="tag__created-at">
+                        <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
+                      </span>
+                    </div>
+                    <h3 className="blog-title">Become fullstack developer</h3>
+                    <a href="" className="blog-detail-btn">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <div className="blog-item">
+                  <div className="img-blog">
+                    <img
+                      src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
+                      alt="blog"
+                    />
+                  </div>
+                  <div className="blog-item__content">
+                    <div className="blog-item__tag">
+                      <span className="tag__category">Design</span>
+                      <span className="tag__created-at">
+                        <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
+                      </span>
+                    </div>
 
-                  <h3 className="blog-title">Become fullstack developer</h3>
-                  <a href="" className="blog-detail-btn">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              {" "}
-              <div className="blog-item">
-                <div className="img-blog">
-                  <img
-                    src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
-                    alt="blog"
-                  />
-                </div>
-                <div className="blog-item__content">
-                  <div className="blog-item__tag">
-                    <span className="tag__category">Design</span>
-                    <span className="tag__created-at">
-                      <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
-                    </span>
+                    <h3 className="blog-title">Become fullstack developer</h3>
+                    <a href="" className="blog-detail-btn">
+                      Read More
+                    </a>
                   </div>
-
-                  <h3 className="blog-title">Become fullstack developer</h3>
-                  <a href="" className="blog-detail-btn">
-                    Read More
-                  </a>
                 </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <div className="blog-item">
+                  <div className="img-blog">
+                    <img
+                      src="https://assets.materialup.com/uploads/0c074539-1537-4a54-8958-05a1e7ba3fd8/preview.png"
+                      alt="blog"
+                    />
+                  </div>
+                  <div className="blog-item__content">
+                    <div className="blog-item__tag">
+                      <span className="tag__category">Design</span>
+                      <span className="tag__created-at">
+                        <FontAwesomeIcon icon={faTimesSquare} /> 13 Jan 2022
+                      </span>
+                    </div>
+
+                    <h3 className="blog-title">Become fullstack developer</h3>
+                    <a href="" className="blog-detail-btn">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </Fade>
       </section>
     </div>
   );
